c1802938108b46896f0656b96e68cc31a24d5ee9
Paul Podgorsek <ppodgorsek@lefoodlink.com>
2025-11-17 21:34:44 +0000

    Issue #13 - Add log messages to better follow the agent's execution

--- scripts/configure-and-run-agent.sh
+++ scripts/configure-and-run-agent.sh
@@ -2,12 +2,18 @@
 
 set -ex
 
+echo "Starting the Azure Pipelines agent..."
+
 if [ -n "${AZURE_DEVOPS_AGENT_NAME}" ]
 then
+  echo "Using a predefined agent name"
   AGENT_NAME="${AZURE_DEVOPS_AGENT_NAME}"
 else
+  echo "Using the host name as the agent name"
   AGENT_NAME="${HOSTNAME}"
 fi
+
+echo "Configuring the '${AGENT_NAME}' agent"
 
 ${AGENT_WORK_DIR}/config.sh \
     --unattended \
@@ -20,13 +26,17 @@ ${AGENT_WORK_DIR}/config.sh \
     --agent "${AGENT_NAME}" \
     --pool "${AZURE_DEVOPS_AGENT_POOL}"
 
+echo "Agent has been configured successfully!"
+
 if [ "${AZURE_DEVOPS_AGENT_PLACEHOLDER_MODE}" == "true" ]
 then
   # The placeholder mode allows to initialise a pool of self-hosted agents
   # See https://github.com/Azure-Samples/container-apps-ci-cd-runner-tutorial/blob/main/azure-pipelines-agent/start.sh
-  echo 'Running in placeholder mode, skipping running the agent'
+  echo "Running in placeholder mode, skipping running the agent"
 else
   # To be aware of TERM and INT signals call run.sh
-  # Running it with the --once flag at the end will shut down the agent after the build is executed
+  echo "Running the agent with the --once flag, this will shut down the agent after the build is executed"
   ${AGENT_WORK_DIR}/run.sh --once
 fi
+
+echo "The agent ran successfully"
