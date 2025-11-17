847a834b2a97f325a17f490691ab64daf7592267
Paul Podgorsek <ppodgorsek@lefoodlink.com>
2025-11-17 17:26:08 +0000

    Issue #3 - Agents should be able to run in placeholder mode

--- scripts/configure-and-run-agent.sh
+++ scripts/configure-and-run-agent.sh
@@ -19,4 +19,13 @@ ${AGENT_WORK_DIR}/config.sh \
     --agent "${AGENT_NAME}" \
     --pool "${AZURE_DEVOPS_AGENT_POOL}"
 
-${AGENT_WORK_DIR}/run.sh --once
+if [ "${AZURE_DEVOPS_AGENT_PLACEHOLDER_MODE}" == "true" ]
+then
+  # The placeholder mode allows to initialise a pool of self-hosted agents
+  # See https://github.com/Azure-Samples/container-apps-ci-cd-runner-tutorial/blob/main/azure-pipelines-agent/start.sh
+  echo 'Running in placeholder mode, skipping running the agent'
+else
+  # To be aware of TERM and INT signals call run.sh
+  # Running it with the --once flag at the end will shut down the agent after the build is executed
+  ${AGENT_WORK_DIR}/run.sh --once
+fi
